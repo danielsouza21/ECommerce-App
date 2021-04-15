f56f20b446a87e34bd16dce6f86c6a2a1c1ed325
Daniel Oliveira Souza <daniel.souza@take.net>
2021-04-15 17:50:19 -0300

    created controller

---
+++ backend/API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
