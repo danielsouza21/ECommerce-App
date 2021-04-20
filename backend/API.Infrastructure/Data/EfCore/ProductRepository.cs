69d6d7c520236355814506469b505782e1e46a12
Daniel Oliveira Souza <daniel.souza@take.net>
2021-04-20 18:59:24 -0300

    loading brands and types when called products list

--- backend/API.Infrastructure/Data/EfCore/ProductRepository.cs
+++ backend/API.Infrastructure/Data/EfCore/ProductRepository.cs
@@ -22,12 +22,20 @@ namespace API.Infrastructure.Data.EfCore
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            var products = _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+
+            return await products;
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
